cb604c718ae55600016b2ebb5559d64fbae84051
KeremCanKirpar <89163711+KeremCanKirpar@users.noreply.github.com>
2024-08-17 15:05:01 +0300

    2.Hafta Yaptıklarımız

---
+++ C# Programlama/Proje03_Variables/Proje03_Variables/Program.cs
@@ -0,0 +1,23 @@
+﻿// namespace Proje03_Variables;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // int degiskenAdi = 56;
+        // int degiskenAdi2;
+        // System.Console.WriteLine(degiskenAdi);
+
+        /* Vs Code Kısayolları;
+        LineCommand = Ctrl + ö, Ctrl + k c
+        BlockCommand = Shift + alt + A
+        Uncommand  = Ctrl + k u */
+
+        //Tam Sayılar
+        sbyte studentPoint = 127;
+        System.Console.WriteLine(studentPoint);
+       
+
+
+    }
+}
