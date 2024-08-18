263398b21fded808a019d72eac1bee2c81586b88
KeremCanKirpar <89163711+KeremCanKirpar@users.noreply.github.com>
2024-08-18 14:22:52 +0300

    Devam

--- C# Programlama/Proje04_ConvertTypes/Proje04_ConvertTypes/Program.cs
+++ C# Programlama/Proje04_ConvertTypes/Proje04_ConvertTypes/Program.cs
@@ -4,27 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        #region Imlict Convert(Örtülü Dönüştürme)
-        /*   int myAge = 21;
-          long newMyAge = myAge; //int to long
+    #region Imlict Convert(Örtülü Dönüştürme)
+    /*   int myAge = 21;
+      long newMyAge = myAge; //int to long
 
-          float rate=0.87f;
-          double newRate = rate; // float to double
+      float rate=0.87f;
+      double newRate = rate; // float to double
 
-          byte studentNote = 75;
-          int newStudentNote = studentNote; // byte to int */
+      byte studentNote = 75;
+      int newStudentNote = studentNote; // byte to int */
 
-        #endregion
+    #endregion
 
-        #region Explicit Convert(Açık Dönüştürme)
+    #region Explicit Convert(Açık Dönüştürme)
+    // double myValue = 123.58;
+    // int newMyValue = (int)myValue; // Cast etmek ya da Unboxing
+    // System.Console.WriteLine("Double: " + myValue + "\n" + "int: " + newMyValue);
 
-        #endregion
-
-        double myValue = 123.58;
-        int newMyValue = (int)myValue; // Cast etmek ya da Unboxing
-        System.Console.WriteLine("Double: " + myValue + "\n" + "int: " + newMyValue);
+    string numberString = "125";
+    int result = Convert.ToInt32(numberString) + 58;
+    System.Console.WriteLine(result);
 
 
+    #endregion
 
-    }
+
+
+
+  }
 }
