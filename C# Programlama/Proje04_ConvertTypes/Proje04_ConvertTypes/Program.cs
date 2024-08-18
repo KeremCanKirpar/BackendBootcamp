c0520c1a54e2a28307953278e3a1f2e474d9fb82
KeremCanKirpar <89163711+KeremCanKirpar@users.noreply.github.com>
2024-08-18 14:04:57 +0300

    2.Hafta Devamı

---
+++ C# Programlama/Proje04_ConvertTypes/Proje04_ConvertTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Proje04_ConvertTypes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Imlict Convert(Örtülü Dönüştürme)
+        /*   int myAge = 21;
+          long newMyAge = myAge; //int to long
+
+          float rate=0.87f;
+          double newRate = rate; // float to double
+
+          byte studentNote = 75;
+          int newStudentNote = studentNote; // byte to int */
+
+        #endregion
+
+        #region Explicit Convert(Açık Dönüştürme)
+
+        #endregion
+
+        double myValue = 123.58;
+        int newMyValue = (int)myValue; // Cast etmek ya da Unboxing
+        System.Console.WriteLine("Double: " + myValue + "\n" + "int: " + newMyValue);
+
+
+
+    }
+}
