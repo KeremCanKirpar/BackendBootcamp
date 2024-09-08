7cff6d0649b8e6c902a2d683555d71e26a7a95ed
KeremCanKirpar <89163711+KeremCanKirpar@users.noreply.github.com>
2024-09-08 15:03:31 +0300

    güncellemeler ile birlikte yeni hali

---
+++ C# Programlama/Week05/08-09-2024/Project17_MethodOverloading/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Project17_MethodOverloading;
+
+class Program
+{
+  static int Sum(int a, int b)
+  {
+    return a+b;
+  }
+  static int Sum(int a, int b, int c,int d)
+  {
+    return a+b+c+d;
+  }
+
+
+    static void Main(string[] args)
+    {
+
+    }
+}
