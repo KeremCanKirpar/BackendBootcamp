50dc6fe9b07fe7745acdf24fe88d33a408e56c72
KeremCanKirpar <89163711+KeremCanKirpar@users.noreply.github.com>
2024-10-06 15:01:05 +0300

    Html,Css ve JavaScript konularını İşledik

---
+++ C# Programlama/HTML/04-JAVASCRIPT/script.js
@@ -0,0 +1,6 @@
+document.getElementById('button-external').addEventListener('mouseover',function(){
+    alert('İşte Üzerine Geldin');
+});
+document.getElementById('button-external').addEventListener('dblclick',function(){
+    alert('Çift tıkladın')
+});
