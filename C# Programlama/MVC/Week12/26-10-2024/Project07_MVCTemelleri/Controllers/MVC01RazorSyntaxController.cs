baa1ed54f4ff93bfd832e37d9fcd5f54ed68f5b6
KeremCanKirpar <89163711+KeremCanKirpar@users.noreply.github.com>
2024-10-26 15:01:49 +0300

    hazır şablonlar ile web sitesi yapmaya başladık

---
+++ C# Programlama/MVC/Week12/26-10-2024/Project07_MVCTemelleri/Controllers/MVC01RazorSyntaxController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project07_MVCTemelleri.Controllers
+{
+    public class MVC01RazorSyntaxController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ C# Programlama/MVC/Week12/26-10-2024/Project07_MVCTemelleri/Controllers/MVC02HtmlAndTagHelpersController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project07_MVCTemelleri.Controllers
+{
+    public class MVC02HtmlAndTagHelpersController : Controller
+    {
+        // GET: MVC02HtmlAndTagHelpersController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
