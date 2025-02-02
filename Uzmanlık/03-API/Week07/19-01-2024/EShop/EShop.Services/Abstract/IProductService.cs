06a5689cddb3aeff130e2834879077c95adfb67d
KeremCanKirpar <89163711+KeremCanKirpar@users.noreply.github.com>
2025-02-02 15:06:04 +0300

    controller işlemleri tamamlandı

--- Uzmanlık/03-API/Week07/19-01-2024/EShop/EShop.Services/Abstract/IProductService.cs
+++ Uzmanlık/03-API/Week07/19-01-2024/EShop/EShop.Services/Abstract/IProductService.cs
@@ -10,6 +10,11 @@ public interface IProductService
     Task<ResponseDto<ProductDto>> GetAsync(int id);
     Task<ResponseDto<ProductDto>> AddAsync(ProductCreateDto productCreateDto);
     Task<ResponseDto<NoContent>> UpdateAsync(ProductUpdateDto productUpdateDto);
-
-    
+    Task<ResponseDto<NoContent>> SoftDeleteAsync(int id);
+    Task<ResponseDto<NoContent>> HardDeleteAsync(int id);
+    Task<ResponseDto<IEnumerable<ProductDto>>> GetAllAsync();
+    Task<ResponseDto<IEnumerable<ProductDto>>> GetAllAsync(bool? isActive);
+    Task<ResponseDto<IEnumerable<ProductDto>>> GetAllAsync(Expression<Func<ProductDto, bool>> filter);
+    Task<ResponseDto<IEnumerable<ProductDto>>> GetProductsByCategoryAsync(int categoryId);
+    Task<ResponseDto<IEnumerable<ProductDto>>> GetProductsByCategoryAsync(int categoryId, Expression<Func<ProductDto, bool>> filter);
 }
---
+++ Uzmanlık/03-API/Week09/01-02-2025/EShop/EShop.API/Controllers/CartController.cs
@@ -0,0 +1,57 @@
+using EShop.Services.Abstract;
+using EShop.Shared.ControllerBases;
+using EShop.Shared.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.API.Controllers
+{
+    [Route("api/carts")]
+    [ApiController]
+    public class CartController : CustomControllerBase
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetCart()
+        {
+            var response = await _cartService.GetCartAsync(GetUserId());
+            return CreateResult(response);
+        }
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> AddtoCart([FromBody] CartItemCreateDto cartItemCreateDto)
+        {
+            var response = await _cartService.AddToCartAsync(cartItemCreateDto);
+            return CreateResult(response);
+        }
+        [Authorize]
+        [HttpDelete("remove/{cartItemId}")]
+        public async Task<IActionResult> RemoveCart(int cartItemId)
+        {
+            var response = await _cartService.RemoveFromCartAsync(cartItemId);
+            return CreateResult(response);
+        }
+        [Authorize]
+        [HttpPut("quantity")]
+        public async Task<IActionResult>ChangeQuantity([FromBody] CartItemUpdateDto cartItemUpdateDto)
+        {
+            var response = await _cartService.ChangeQuantityAsync(cartItemUpdateDto);
+            return CreateResult(response);
+        }
+        [Authorize]
+        [HttpDelete("clear")]
+        public async Task<IActionResult> Clear()
+        {
+            var response = await _cartService.ClearCartAsync(GetUserId());
+            return CreateResult(response);
+        }
+
+    }
+}
